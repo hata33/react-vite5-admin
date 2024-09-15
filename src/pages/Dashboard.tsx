c49aac085b0157bcb07ef79d34fbd59f37e9b10c
zhaowenzhe <1553126902@qq.com>
2024-09-16 00:06:03 +0800

    feat: 新增路由，tailwind

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { SvgIcon } from '@/components/icons';
+
+function Dashboard() {
+  return (
+    <div>
+      <SvgIcon icon="ic-user" size="20px" color="#FC78C4" />
+    </div>
+  );
+}
+
+export default Dashboard;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: './',
+  esbuild: {},
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [
+    react(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '#': path.resolve(__dirname, '/types'),
+    },
+  },
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'https://xxxx',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
