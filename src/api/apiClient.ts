3431f6f08ad82f0c479c311d256080d0fe8e355e
zhaowenzhe <1553126902@qq.com>
2024-09-16 18:59:18 +0800

    feat: 新增axios 网络请求

---
+++ src/api/apiClient.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosRequestConfig, AxiosError, AxiosResponse } from 'axios';
+import { Result } from '#/axios';
+
+// 创建 axios 实例
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_BASE_API as string,
+  timeout: 50000,
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = 'Bearer Token';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+class APIClient {
+  get<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({ ...config, method: 'GET' });
+  }
+
+  post<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({ ...config, method: 'POST' });
+  }
+  put<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({ ...config, method: 'PUT' });
+  }
+  delete<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return this.request({ ...config, method: 'DELETE' });
+  }
+  request<T = any>(config: AxiosRequestConfig): Promise<T> {
+    return new Promise((resolve, reject) => {
+      axiosInstance
+        .request<any, AxiosResponse<Result>>(config)
+        .then((res: AxiosResponse<Result>) => {
+          resolve(res as unknown as Promise<T>);
+        })
+        .catch((e: Error | AxiosError) => {
+          reject(e);
+        });
+    });
+  }
+}
+
+export default new APIClient();
