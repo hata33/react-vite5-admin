79cbf028eb6318823b67e068541508baf861625b
zhaowenzhe <1553126902@qq.com>
2024-09-22 23:20:29 +0800

    feat: top progress bar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,30 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+
+import { Progress } from '@/components/app';
 
 import Content from './content';
 import Header from './header';
 import SiderBar from './sidebar';
 
 function BasicLayout() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 50);
+    return () => setIsLoading(true);
+  }, [pathname]);
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
   return (
     <div>
+      <Progress isAnimating={isLoading} />
       <div className="flex h-screen overflow-hidden">
         <div className="hidden border-r-[1px] border-dashed border-r-[#919eab33] lg:block">
           <SiderBar />
