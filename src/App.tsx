d7001516ad63e0ede07b68eceaabf286ac408aa2
zhaowenzhe <1553126902@qq.com>
2024-10-02 16:17:19 +0800

    refactor: themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
-import { StyleProvider } from '@ant-design/cssinjs';
-import { ConfigProvider, theme } from 'antd';
 import { RouterProvider } from 'react-router-dom';
 
 import router from '@/router';
 
-import { useSettings } from './store/settingStore';
-import { colorPrimarys, customAntdTheme } from './theme/antd/theme';
+import ThemeProvider from '@/theme';
 
-import { ThemeMode } from '#/enum';
 function App() {
-  const settings = useSettings();
-
-  const { themeMode, themeColorPresets } = settings;
-  const algorithm = themeMode === ThemeMode.Light ? theme.defaultAlgorithm : theme.darkAlgorithm;
-
-  const colorPrimary = colorPrimarys[themeColorPresets];
-
   return (
-    <ConfigProvider
-      theme={{
-        token: { ...customAntdTheme.token, colorPrimary },
-        components: { ...customAntdTheme.components },
-        algorithm,
-      }}
-    >
-      <StyleProvider hashPriority="high">
-        <RouterProvider router={router} />
-      </StyleProvider>
-    </ConfigProvider>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
